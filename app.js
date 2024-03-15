f54f2c7ecb7c36df9ccda38771265afda7e7a278
yuvasaiganesh <yuvasaiganesh@gmail.com>
2024-03-15 22:25:06 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
+const path = require("path");
+const cors=require("cors")
+
+const app = express();
+app.use(express.json());
+app.use(cors({origin:true,credentials:true}));
+module.exports = app;
+
+const dbPath = path.join(__dirname, "details.db");
+
+let db = null;
+
+const initializeServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(5000, () => {
+      console.log("Server Running Successfully at http://localhost:5000");
+    });
+  } catch (e) {
+    console.log(`Error ${e}`);
+    process.exit(1);
+  }
+};
+
+initializeServer();
+
+//API 1
+app.get("/", async (request,response) => {
+  
+ const todoRequest = `
+        SELECT * 
+        FROM products 
+        `;
+
+  const allproducts = await db.all(todoRequest);
+  console.log(allproducts);
+  response.send({ allproducts });
+});
+
+//API 2
